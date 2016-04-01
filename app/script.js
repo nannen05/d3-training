efbf25d1fe1d6407246d2488b63718c7f55688e9
nannen05 <nannen05@gmail.com>
2016-03-31 18:53:53 -0700

    added enter method

--- app/script.js
+++ app/script.js
@@ -1,50 +1,43 @@
-d3.selectAll('.item:nth-child(3)')
-	.classed({
-		'highlight' : true, 
-		'item' : false,
-		'bigger' : true
-	});
-
-d3.selectAll('.item:nth-child(4)')
-	.attr('class', 'bigger')
-
-d3.selectAll('.item:nth-child(5)')
-	.style({
-		'padding' : '10px',
-		'margin' : '5px',
-		'color' : '#EEE8D5'		
-		})
-
-
 var myStyles = [
 	{
 		'color' : '#268BD2',
+		'name': 'Barot',
 		'width' : 220
 	}, 
 	{
 		'color' : '#BD3613',
+		'name': 'Hassum',
 		'width' : 200
 	},
 	{
 		'color' : '#D11c24',
+		'name': 'Jeniffer',
 		'width' : 230
 	},
 	{
 		'color' : '#c61c6f',
+		'name': 'Richard',
 		'width' : 245
 	},
 	{
 		'color' : '#595Ab7',
+		'name': 'Lorenzo',
 		'width' : 205
 	},
 	{
 		'color' : '#2176c7',
+		'name': 'Xhou',
 		'width' : 222
 	}
 	];
 
-d3.selectAll('.item')
+d3.selectAll('#chart').selectAll('div')
 	.data(myStyles)
+	.enter().append('div')
+	.classed('item', true)
+	.text(function(d) {
+		return d.name;
+	})
 	.style({
 		'color' : '#ffffff',
 		'background' : function(d) {
