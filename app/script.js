eb802659ef87cab67b9ada74a80865db8ff752aa
nannen05 <nannen05@gmail.com>
2016-04-04 19:37:53 -0700

    drawing svgs with d3

--- app/script.js
+++ app/script.js
@@ -48,5 +48,23 @@ d3.selectAll('#chart').selectAll('div')
 		}
 	});
 
+d3.select('#chart-01')
+	.append('svg')
+		.attr('width', 600)
+		.attr('height', 400)
+		.style('background', "#93a1a1")
+	.append('rect')
+		.attr('x', 200)
+		.attr('y', 100)
+		.attr('width', 200)
+		.attr('height', 200)
+		.style('fill', "#cb4b19")
+	d3.select('svg')
+		.append('circle')
+		.attr('cx', '300')
+		.attr('cy', '200')
+		.attr('r', 50)
+		.style('fill', '#840043')
+
 
 
